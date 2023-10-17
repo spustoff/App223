672f0e3c1e904d0879ac9370d6dc0a0a36e03419
spustoff <vyacheslavpustov05@gmail.com>
2023-10-17 15:02:20 +0300

    Initial Commit

---
+++ App223/App223App.swift
@@ -0,0 +1,17 @@
+//
+//  App223App.swift
+//  App223
+//
+//  Created by Вячеслав on 10/17/23.
+//
+
+import SwiftUI
+
+@main
+struct App223App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App223/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  App223
+//
+//  Created by Вячеслав on 10/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
